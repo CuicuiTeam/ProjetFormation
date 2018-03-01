1857af3c31a7db39bd03237c1866a581c059f3d5
ango <antoine.gobert@infotel.com>
2018-03-01 15:55:07 +0100

    Ajout du Livre

---
+++ ProjetFormation-domain/src/main/java/com/formation/entities/Livre.java
@@ -0,0 +1,140 @@
+package com.formation.entities;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Livre implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	//test
+	private String titre;
+	private String description;
+	private double prix;
+	private Date datePublication;
+	private String imagePath;
+	private boolean isPopular;
+	private boolean isPeriodic;
+
+	@ManyToMany
+	private List<Auteur> auteurs;
+
+	@ManyToOne
+	private Editeur editeur;
+
+	@ManyToOne
+	private Categorie categorie;
+
+	public Editeur getEditeur() {
+		return editeur;
+	}
+
+	public void setEditeur(Editeur editeur) {
+		this.editeur = editeur;
+	}
+
+	public Livre() {
+
+	}
+
+	public Livre(String titre, String description, float prix, Date datePublication, String imagePath,
+			boolean isPopular, boolean isPeriodic) {
+
+		this.titre = titre;
+		this.description = description;
+		this.prix = prix;
+		this.datePublication = datePublication;
+		this.imagePath = imagePath;
+		this.isPopular = isPopular;
+		this.isPeriodic = isPeriodic;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDatePublication() {
+		return datePublication;
+	}
+
+	public void setDatePublication(Date datePublication) {
+		this.datePublication = datePublication;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+	public boolean isPopular() {
+		return isPopular;
+	}
+
+	public void setPopular(boolean isPopular) {
+		this.isPopular = isPopular;
+	}
+
+	public boolean isPeriodic() {
+		return isPeriodic;
+	}
+
+	public void setPeriodic(boolean isPeriodic) {
+		this.isPeriodic = isPeriodic;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	public List<Auteur> getAuteurs() {
+		return auteurs;
+	}
+
+	public void setAuteurs(List<Auteur> auteurs) {
+		this.auteurs = auteurs;
+	}
+
+	public Categorie getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+
+}
--- ProjetFormation-domain/src/main/java/com/formation/entities/Panier.java
+++ ProjetFormation-domain/src/main/java/com/formation/entities/Panier.java
@@ -3,11 +3,13 @@ package com.formation.entities;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+@Entity
 public class Panier implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
