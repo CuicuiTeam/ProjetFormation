0133138fc48a175d2a3b76220b3b2774f72dd8f0
Lena <lena.letexier@infotel.com>
2018-03-05 16:41:55 +0100

    ajout periodique jsp + Categorie DAO et Service

--- ProjetFormation-dao/src/main/java/com/formation/implementation/CategorieDAOImpl.java
+++ ProjetFormation-dao/src/main/java/com/formation/implementation/CategorieDAOImpl.java
@@ -1,5 +1,7 @@
 package com.formation.implementation;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,4 +12,11 @@ import com.formation.entities.Categorie;
 @Transactional
 public class CategorieDAOImpl extends DAOPrincipalImpl<Categorie> implements CategorieDAO {
 
+@Autowired
+private SessionFactory sessionFactory;
+
+public Categorie getCategorieByNom(String nom) {
+	return (Categorie)sessionFactory.getCurrentSession().createQuery("FROM Categorie C WHERE C.nom = :nom ORDER BY C.nom").setParameter("nom", nom);
+}
+	
 }
