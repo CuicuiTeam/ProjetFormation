d328b93c0bc5a6affcf7670b3642dd40d5bff9d8
Romain <romain.ronco@infotel.com>
2018-03-05 12:17:21 +0100

    Ajout DAO livre

--- ProjetFormation-dao/src/main/java/com/formation/dao/LivreDAO.java
+++ ProjetFormation-dao/src/main/java/com/formation/dao/LivreDAO.java
@@ -1,8 +1,22 @@
 
 package com.formation.dao;
 
+import java.util.List;
+
+import com.formation.entities.Auteur;
+import com.formation.entities.Categorie;
 import com.formation.entities.Livre;
 
 public interface LivreDAO extends DAOPrincipal<Livre> {
+
+	public List<Livre> getLivreByCat(Categorie cat);
+
+	public List<Livre> getPeriodiques();
+
+	public List<Livre> getLivreByAuteur(Auteur auteur);
+
+	public List<Livre> getLivreByRecherche(String recherche);
+
+	public List<Livre> getLivreRecommandes();
 
 }
--- ProjetFormation-dao/src/main/java/com/formation/implementation/LivreDAOImpl.java
+++ ProjetFormation-dao/src/main/java/com/formation/implementation/LivreDAOImpl.java
@@ -1,9 +1,53 @@
 
 package com.formation.implementation;
 
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.formation.dao.LivreDAO;
+import com.formation.entities.Auteur;
+import com.formation.entities.Categorie;
 import com.formation.entities.Livre;
 
 public class LivreDAOImpl extends DAOPrincipalImpl<Livre> implements LivreDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	public List<Livre> getLivreByCat(Categorie cat) {
+		// TODO Auto-generated method stub
+		return (List<Livre>) sessionFactory.getCurrentSession().createQuery("FROM Livre L WHERE L.categorie = :cat")
+				.setParameter("cat", cat);
+	}
+
+	@Override
+	public List<Livre> getPeriodiques() {
+		// TODO Auto-generated method stub
+		return (List<Livre>) sessionFactory.getCurrentSession().createQuery("FROM Livre L WHERE L.isPeriodic = 1");
+	}
+
+	@Override
+	public List<Livre> getLivreByAuteur(Auteur auteur) {
+		// TODO Auto-generated method stub
+		return (List<Livre>) sessionFactory.getCurrentSession()
+				.createQuery("FROM Livre L WHERE L.auteur =:auteur ORDER BY L.titre").setParameter("auteur", auteur);
+	}
+
+	@Override
+	public List<Livre> getLivreByRecherche(String recherche) {
+		// TODO Auto-generated method stub
+		return (List<Livre>) sessionFactory.getCurrentSession()
+				.createQuery("FROM Livre L WHERE L.titre LIKE :recherche OR L.description LIKE :recherche")
+				.setParameter("recherche", "%" + recherche + "%");
+	}
+
+	@Override
+	public List<Livre> getLivreRecommandes() {
+		// TODO Auto-generated method stub
+		return (List<Livre>) sessionFactory.getCurrentSession().createQuery("FROM Livre L WHERE L.isPopular = 1");
+	}
 
 }
