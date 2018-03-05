5412a38e0576f8e07f3b700f41adf94557d530c7
Romain <romain.ronco@infotel.com>
2018-03-05 12:16:33 +0100

    Ajout module Service

---
+++ ProjectFormation-service/src/main/java/com/formation/service/LivreService.java
@@ -0,0 +1,21 @@
+package com.formation.service;
+
+import java.util.List;
+
+import com.formation.entities.Auteur;
+import com.formation.entities.Categorie;
+import com.formation.entities.Livre;
+
+public interface LivreService {
+
+	public List<Livre> getLivreByCat(Categorie cat);
+
+	public List<Livre> getPeriodiques();
+
+	public List<Livre> getLivreByAuteur(Auteur auteur);
+
+	public List<Livre> getLivreByRecherche(String recherche);
+
+	public List<Livre> getLivreRecommandes();
+
+}
