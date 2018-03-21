d666b9732964c64f81b7ae074ab6fcf6d691816e
Lena <lena.letexier@infotel.com>
2018-03-21 10:05:26 +0100

    membreControlleur

--- ProjetFormation-web/src/main/java/com/formation/controlleur/MembreControlleur.java
+++ ProjetFormation-web/src/main/java/com/formation/controlleur/MembreControlleur.java
@@ -47,7 +47,9 @@ public class MembreControlleur {
 	public Resultat ajoutMembre(@RequestBody MembreDTO membreDto) {
 		Resultat resultat = new Resultat();
 		try {
-			Membre newMembre = new Membre(membreDto.getNom(), membreDto.getPrenom(), membreDto.getEmail(), membreDto.getPassword(), membreDto.getAdresse(), membreDto.getVille(), membreDto.getCodePostal(), membreDto.getTelephone(), membreDto.isAdmin());
+			Membre newMembre = new Membre(membreDto.getNom(), membreDto.getPrenom(), membreDto.getEmail(),
+					membreDto.getPassword(), membreDto.getAdresse(), membreDto.getVille(), membreDto.getCodePostal(),
+					membreDto.getTelephone(), membreDto.isAdmin());
 			newMembre.setInscriptions(inscriptionService.getInscriptionById(membreDto.getInscriptionsId()));
 			membreService.save(newMembre);
 			resultat.setSuccess(true);
@@ -72,14 +74,21 @@ public class MembreControlleur {
 		try {
 			List<Membre> listeMembres = membreService.getAll();
 
-
-			listeMembres.forEach(membre -> {			
-				MembreDTO membreDto = new MembreDTO(membre.getNom(), membre.getPrenom(), membre.getPassword(), membre.getAdresse(), membre.getVille(), membre.getCodePostal(), membre.getTelephone(), membre.getEmail(), membre.isAdmin());
+			listeMembres.forEach(membre -> {
+				MembreDTO membreDto = new MembreDTO(membre.getNom(), membre.getPrenom(), membre.getPassword(),
+						membre.getAdresse(), membre.getVille(), membre.getCodePostal(), membre.getTelephone(),
+						membre.getEmail(), membre.isAdmin());
 				List<Integer> inscriptionIds = new ArrayList<Integer>();
 
 				membre.getInscriptions().forEach(inscription -> inscriptionIds.add(inscription.getId()));
 				membreDto.setInscriptionsId(inscriptionIds);
-				membreDto.setId(membre.getId());
+
+				int test = 0;
+				test = membre.getId();
+				if (test != 0) {
+					membreDto.setId(membre.getId());
+				}
+
 				listeMembre.add(membreDto);
 				resultat.setPayload(listeMembre);
 			});
@@ -98,7 +107,7 @@ public class MembreControlleur {
 	}
 
 	@PostMapping(value = "/admin/membre/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public Resultat updateMembre(@RequestBody MembreDTO membreDto, @PathVariable(value="id") int id){
+	public Resultat updateMembre(@RequestBody MembreDTO membreDto, @PathVariable(value = "id") int id) {
 		Resultat resultat = new Resultat();
 		try {
 			Membre membre = membreService.get(id);
@@ -132,8 +141,8 @@ public class MembreControlleur {
 		return resultat;
 	}
 
-	@DeleteMapping(value="/admin/membre/{id}")
-	private Resultat deleteMembre(@PathVariable(value="id") int id) {
+	@DeleteMapping(value = "/admin/membre/{id}")
+	private Resultat deleteMembre(@PathVariable(value = "id") int id) {
 		Resultat resultat = new Resultat();
 		try {
 			membreService.delete(membreService.get(id));
@@ -153,81 +162,90 @@ public class MembreControlleur {
 	}
 
 	/////////////////////////////////////////////////////////////////////////////////
-	//	@RequestMapping(value = "/toto", method = RequestMethod.GET)
-	//	private String ajoutMembre(Model model) {
+	// @RequestMapping(value = "/toto", method = RequestMethod.GET)
+	// private String ajoutMembre(Model model) {
 	//
-	//		Membre newMembre = new Membre();
-	//		model.addAttribute("newMembre", newMembre);
-	//		return "inscription";
-	//	}
+	// Membre newMembre = new Membre();
+	// model.addAttribute("newMembre", newMembre);
+	// return "inscription";
+	// }
 	//
-	//	@RequestMapping(value = "/toto", method = RequestMethod.POST)
-	//	private String ajoutMembre(@ModelAttribute("newMembre") Membre newMembre, Model model) {
-	//		newMembre.setPassword(membreService.cryptageMdp(newMembre.getPassword()));
-	//		if (membreService.findByEmail(newMembre.getEmail()) != null) {
-	//			model.addAttribute("msgErreur", "Cet email est déjà utilisé");
-	//			newMembre.setPassword("");
-	//			return "inscription";
-	//		} else {
-	//			membreService.save(newMembre);
-	//			return "redirect:/";
-	//		}
-	//	}
+	// @RequestMapping(value = "/toto", method = RequestMethod.POST)
+	// private String ajoutMembre(@ModelAttribute("newMembre") Membre newMembre,
+	///////////////////////////////////////////////////////////////////////////////// Model
+	///////////////////////////////////////////////////////////////////////////////// model)
+	///////////////////////////////////////////////////////////////////////////////// {
+	// newMembre.setPassword(membreService.cryptageMdp(newMembre.getPassword()));
+	// if (membreService.findByEmail(newMembre.getEmail()) != null) {
+	// model.addAttribute("msgErreur", "Cet email est déjà utilisé");
+	// newMembre.setPassword("");
+	// return "inscription";
+	// } else {
+	// membreService.save(newMembre);
+	// return "redirect:/";
+	// }
+	// }
 
-	//	@RequestMapping(value = "/connexion", method = RequestMethod.GET)
-	//	private String connexionMembre(Model model) {
+	// @RequestMapping(value = "/connexion", method = RequestMethod.GET)
+	// private String connexionMembre(Model model) {
 	//
-	//		Membre newMembre = new Membre();
-	//		model.addAttribute("login", newMembre);
-	//		return "connexion";
-	//	}
+	// Membre newMembre = new Membre();
+	// model.addAttribute("login", newMembre);
+	// return "connexion";
+	// }
 	//
-	//	@RequestMapping(value = "/connexioncheck", method = RequestMethod.POST)
-	//	private String connexionMembre(@ModelAttribute("login") Membre newMembre, Model model, HttpServletRequest request) {
-	//		HttpSession session = request.getSession();
-	//		try {
-	//			Membre membre = membreService.identification(newMembre.getEmail(), newMembre.getPassword());
-	//		} catch (ServiceException e) {
-	//			// TODO Auto-generated catch block
-	//			e.printStackTrace();
-	//		}
-	//		if(membre != null) {
-	//			session.setAttribute("user", membre);
-	//			return "redirect:/";
-	//		} else {
-	//			model.addAttribute("msgErreur", "Veuillez saisir un identifiant et un mot de passe valide");
-	//			return "connexion";
-	//		}
-	//	}
+	// @RequestMapping(value = "/connexioncheck", method = RequestMethod.POST)
+	// private String connexionMembre(@ModelAttribute("login") Membre newMembre,
+	// Model model, HttpServletRequest request) {
+	// HttpSession session = request.getSession();
+	// try {
+	// Membre membre = membreService.identification(newMembre.getEmail(),
+	// newMembre.getPassword());
+	// } catch (ServiceException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	// if(membre != null) {
+	// session.setAttribute("user", membre);
+	// return "redirect:/";
+	// } else {
+	// model.addAttribute("msgErreur", "Veuillez saisir un identifiant et un mot de
+	// passe valide");
+	// return "connexion";
+	// }
+	// }
 
-	//	@RequestMapping(value = "/supprimerMembre", method = RequestMethod.GET)
-	//	private String supprMembre(Model model) {
+	// @RequestMapping(value = "/supprimerMembre", method = RequestMethod.GET)
+	// private String supprMembre(Model model) {
 	//
-	//		model.addAttribute("membres", membreService.getAll());
-	//		return "adminmembresuppr";
-	//	}
+	// model.addAttribute("membres", membreService.getAll());
+	// return "adminmembresuppr";
+	// }
 	//
-	//	@RequestMapping(value = "/supprimerMembre", method = RequestMethod.POST)
-	//	private String supprMembre(HttpServletRequest request) {
-	//		Membre membre = membreService.get(Integer.parseInt(request.getParameter("membreId")));
-	//		membreService.delete(membre);
-	//		return "redirect:/supprimerMembre";
+	// @RequestMapping(value = "/supprimerMembre", method = RequestMethod.POST)
+	// private String supprMembre(HttpServletRequest request) {
+	// Membre membre =
+	// membreService.get(Integer.parseInt(request.getParameter("membreId")));
+	// membreService.delete(membre);
+	// return "redirect:/supprimerMembre";
 	//
-	//	}
+	// }
 	//
-	//	@RequestMapping(value = "/editmembre", method = RequestMethod.GET)
-	//	private String editMembre(HttpServletRequest request, Model model) {
-	//		Membre membre = membreService.get(Integer.parseInt(request.getParameter("idMembre")));
-	//		model.addAttribute("edit", membre);
-	//		return "admineditmembre";
+	// @RequestMapping(value = "/editmembre", method = RequestMethod.GET)
+	// private String editMembre(HttpServletRequest request, Model model) {
+	// Membre membre =
+	// membreService.get(Integer.parseInt(request.getParameter("idMembre")));
+	// model.addAttribute("edit", membre);
+	// return "admineditmembre";
 	//
-	//	}
+	// }
 	//
-	//	@RequestMapping(value = "/editmembre", method = RequestMethod.POST)
-	//	private String editMembre(@ModelAttribute("edit") Membre membre, @RequestParam(value = "idMembre") int idMembre) {
-	//		membre.setId(idMembre);
-	//		membreService.save(membre);
-	//		return "redirect:/supprimerMembre";
+	// @RequestMapping(value = "/editmembre", method = RequestMethod.POST)
+	// private String editMembre(@ModelAttribute("edit") Membre membre,
+	// @RequestParam(value = "idMembre") int idMembre) {
+	// membre.setId(idMembre);
+	// membreService.save(membre);
+	// return "redirect:/supprimerMembre";
 	//
-	//	}
+	// }
 }
