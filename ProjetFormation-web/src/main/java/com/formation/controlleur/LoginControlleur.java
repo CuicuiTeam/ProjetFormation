84bbc6c6917caed848b400215fedf138bc375783
ango <antoine.gobert@infotel.com>
2018-03-21 16:57:04 +0100

    Login + livre

--- ProjetFormation-web/src/main/java/com/formation/controlleur/LoginControlleur.java
+++ ProjetFormation-web/src/main/java/com/formation/controlleur/LoginControlleur.java
@@ -1,5 +1,7 @@
 package com.formation.controlleur;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
