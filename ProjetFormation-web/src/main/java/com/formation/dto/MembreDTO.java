e8e1418bf0427c71c1b52a2947b7ec067347f7fe
ango <antoine.gobert@infotel.com>
2018-03-22 11:46:05 +0100

    Ajout de la session, modification de plusieurs DTO, ajout de la deconnexion

--- ProjetFormation-web/src/main/java/com/formation/dto/MembreDTO.java
+++ ProjetFormation-web/src/main/java/com/formation/dto/MembreDTO.java
@@ -1,12 +1,12 @@
 package com.formation.dto;
 
-import java.io.Serializable;
 import java.util.List;
 
 
 public class MembreDTO {
 	
 	private List<Integer> inscriptionsId;
+	private List<Integer> bibliothequesId;
 	private int id;
 	private String nom;
 	private String prenom;
@@ -23,9 +23,9 @@ public class MembreDTO {
 	}
 
 
-	public MembreDTO( String nom, String prenom, String password, String adresse,
+	public MembreDTO(int id, String nom, String prenom, String password, String adresse,
 			String ville, String codePostal, String telephone, String email, boolean isAdmin) {
-		this.inscriptionsId = inscriptionsId;
+		this.id = id;
 		this.nom = nom;
 		this.prenom = prenom;
 		this.password = password;
@@ -148,9 +148,15 @@ public class MembreDTO {
 	}
 
 
-	
+	public List<Integer> getBibliothequesId() {
+		return bibliothequesId;
+	}
 
-	
+
+	public void setBibliothequesId(List<Integer> bibliothequesId) {
+		this.bibliothequesId = bibliothequesId;
+	}
+
 	
 	
 }
